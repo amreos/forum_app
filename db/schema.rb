30e35e3b37c186e14abf59fe3c1d4fde01451ae9
Amr Samir <amr.samier@gmail.com>
2018-05-11 09:27:23 +0000

    null allow

--- db/schema.rb
+++ db/schema.rb
@@ -52,11 +52,11 @@ ActiveRecord::Schema.define(version: 20180202063252) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name", default: "admin"
+    t.string "resource_type", default: "admin"
+    t.integer "resource_id", default: "1"
+    t.datetime "created_at",  null: true
+    t.datetime "updated_at", null: true
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
